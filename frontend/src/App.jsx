1c17ca919442d9207dfe092a1711db9f4be42e6d
Hitesh <hiteshghanchi87@gmail.com>
2025-01-29 13:09:48 +0530

    Yo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import ConnectWallet from '';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path='/' element={<></>}></Route> */}</Routes>
+      <Routes><Route path='/' element={<ConnectWallet></ConnectWallet>}></Route></Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/ConnectWallet.jsx
+++ frontend/src/ConnectWallet.jsx
@@ -23,7 +23,7 @@ export default function ConnectWallet() {
     }
 
     try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const provider = new ethers.BrowserProvider(window.ethereum);
       await provider.send("eth_requestAccounts", []);
       const signer = provider.getSigner();
       const address = await signer.getAddress();
